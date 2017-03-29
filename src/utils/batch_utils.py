f79a02c769687d99a2c1f4e35e83ebf5de420a44
Saurabh Mathur <saurabhmathur96@gmail.com>
2017-03-29 14:52:39 +0530

    reduce RAM usage

--- src/utils/batch_utils.py
+++ src/utils/batch_utils.py
@@ -11,14 +11,9 @@ class BatchIterator(object):
         self.batch_size = batch_size
         self.one_hot_target = one_hot_target
 
-        inverse_vocabulary = dict((word, i) for i, word in enumerate(self.vocabulary))
-        q = ([inverse_vocabulary[word] for word in question.split()] for question in questions)
-        a = ([inverse_vocabulary[word] for word in answer.split()] for answer in answers)
-        
-        self.X = pad_sequences(q, maxlen=self.sequence_length)
-        self.y = pad_sequences(a, maxlen=self.sequence_length)
-
-    
+        self.questions = questions
+        self.answers = answers
+        self.inverse_vocabulary = dict((word, i) for i, word in enumerate(self.vocabulary))
     def to_one_hot(self, y):
         out = zeros(shape=(self.batch_size, self.sequence_length, len(self.vocabulary)), dtype=bool)
         for batch in range(self.batch_size):
@@ -29,10 +24,17 @@ class BatchIterator(object):
     def next_batch(self):
         n_example = self.X.shape[0]
         indices = random.randint(0, n_example, size=(self.batch_size))
+        inverse_vocabulary = self.inverse_vocabulary
+        q = [[inverse_vocabulary[word] for word in self.questions[i].split()] for i in indices]
+        a = [[inverse_vocabulary[word] for word in self.answers[i].split()] for i in indices]
+        
+        X = pad_sequences(q, maxlen=self.sequence_length)
+        y = pad_sequences(a, maxlen=self.sequence_length)
+
         if self.one_hot_target:
-            return (self.X[indices], self.to_one_hot(self.y[indices]))
+            return (X[indices], self.to_one_hot(y[indices]))
         else:
-            return (self.X[indices], self.y[indices])
+            return (X[indices], y[indices])
 
     
     
